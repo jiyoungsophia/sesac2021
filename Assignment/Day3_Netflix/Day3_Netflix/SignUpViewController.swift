61c764eb1e3e2498b42ef211fe2959a58867c823
jiyoung <yangg0228@gmail.com>
2021-10-01 20:47:09 +0900

    update README.md & Assignment

--- Assignment/Day3_Netflix/Day3_Netflix/SignUpViewController.swift
+++ Assignment/Day3_Netflix/Day3_Netflix/SignUpViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class SignUpViewController: UIViewController {
+class SignUpViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet var emailTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
@@ -22,11 +22,13 @@ class SignUpViewController: UIViewController {
         
         super.viewDidLoad()
         
+        
         setTextFieldUI(emailTextField, placeHolder: "이메일 주소 또는 전화번호", keyBoard: .emailAddress)
         setTextFieldUI(passwordTextField, placeHolder: "비밀번호", secureText: true)
         setTextFieldUI(nicknameTextField, placeHolder: "닉네임")
         setTextFieldUI(locationTextField, placeHolder: "위치")
         setTextFieldUI(codeTextField, placeHolder: "추천 코드 입력", keyBoard: .numberPad)
+        codeTextField.delegate = self
         
         setUI()
     }
@@ -35,11 +37,14 @@ class SignUpViewController: UIViewController {
         tf.placeholder = ph
         tf.textColor = .white
         tf.keyboardType = kb
+        // 패스워드 키체인 오류
+        tf.textContentType = .oneTimeCode
         tf.isSecureTextEntry = st
         tf.textAlignment = .center
         tf.borderStyle = .roundedRect
         tf.backgroundColor = .gray
     }
+    
     
     func setUI(){
         signUpButton.setTitle("회원가입", for: .normal)
@@ -52,6 +57,11 @@ class SignUpViewController: UIViewController {
         infoSwitch.thumbTintColor = .white
     }
     
+    // 코드 텍스트필드 숫자만
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+            let invalidCharacters = CharacterSet(charactersIn: "0123456789").inverted
+            return string.rangeOfCharacter(from: invalidCharacters) == nil
+        }
     
     
     @IBAction func tapClicked(_ sender: UITapGestureRecognizer) {
@@ -60,14 +70,38 @@ class SignUpViewController: UIViewController {
         
     }
     
+    
     @IBAction func signUpClicked(_ sender: UIButton) {
-        print("회원 가입 정보 확인")
-        print("ID: \(emailTextField.text ?? "")")
-        print("PW: \(passwordTextField.text ?? "")")
+        
+        var userEmail : String = ""
+        var userPwd : String = ""
+        
+        userEmail = emailTextField.text ?? ""
+        userPwd = passwordTextField.text ?? ""
+        
+        if ((userEmail.isEmpty) != true) {
+            print("ID: \(userEmail)")
+        } else {
+            print("이메일을 입력하세요")
+        }
+        
+        if ((userPwd.isEmpty) != true) {
+            if userPwd.count >= 6 {
+                print("PW: \(userPwd)")
+            } else {
+                print("비밀번호를 6자리 이상 입력하세요")
+            }
+        } else {
+            print("비밀번호를 입력하세요")
+        }
+        
+        
         print("NICK: \(nicknameTextField.text ?? "")")
         print("LOCATION: \(locationTextField.text ?? "")")
         print("CODE: \(codeTextField.text ?? "")")
+        
     }
+
     
  
     @IBAction func infoSwitchClicked(_ sender: UISwitch) {
