53098ab50190e20034be58737d9a83b4cf57fd1e
jiyoung <yangg0228@gmail.com>
2021-10-04 22:48:42 +0900

    add Assignment

--- Assignment/Day5_LEDBoard/Day5_LEDBoard/ViewController.swift
+++ Assignment/Day5_LEDBoard/Day5_LEDBoard/ViewController.swift
@@ -9,11 +9,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var boardView: UIView!
+    @IBOutlet var userTextField: UITextField!
+    @IBOutlet var sendButton: UIButton!
+    @IBOutlet var textColorButton: UIButton!
+    @IBOutlet var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setUI()
     }
 
-
+    func setUI() {
+        userTextField.placeholder = "텍스트를 입력하세요"
+        
+        boardView.layer.cornerRadius = 15
+        
+        sendButton.layer.cornerRadius = 15
+        sendButton.layer.borderWidth = 2
+        sendButton.layer.borderColor = UIColor.black.cgColor
+        sendButton.setTitleColor(.black, for: .normal)
+        
+        textColorButton.layer.cornerRadius = 15
+        textColorButton.layer.borderWidth = 2
+        textColorButton.layer.borderColor = UIColor.black.cgColor
+        textColorButton.setTitleColor(.red, for: .normal)
+        
+        resultLabel.numberOfLines = 0
+        resultLabel.textAlignment = .center
+        resultLabel.adjustsFontSizeToFitWidth = true
+        resultLabel.minimumScaleFactor = 0.1
+    }
+    
+    @IBAction func clickSendButton(_ sender: UIButton) {
+        resultLabel.text = userTextField.text
+    }
+    
+    @IBAction func clickTextColorButton(_ sender: UIButton) {
+        resultLabel.textColor = .white
+    }
+    
+    func random() -> UIColor {
+        return UIColor(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: 1.0)
+        }
+    
+    
+    @IBAction func clickTap(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+        
+        if boardView.isHidden == false {
+            boardView.isHidden = true
+        } else {
+            boardView.isHidden = false
+        }
+    }
+    
+    @IBAction func clickReturnKey(_ sender: UITextField) {
+        view.endEditing(true)
+    }
+    
+    
 }
 
