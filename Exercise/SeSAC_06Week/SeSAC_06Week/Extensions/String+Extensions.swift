86391bc04ac1dfffc1440cbe54ee8b6c106a36e2
jiyoung <yangg0228@gmail.com>
2021-11-01 16:09:35 +0900

    update Exercise24

---
+++ Exercise/SeSAC_06Week/SeSAC_06Week/Extensions/String+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  String+Extensions.swift
+//  SeSAC_06Week
+//
+//  Created by 지영 on 2021/11/01.
+//
+
+import Foundation
---
+++ Exercise/SeSAC_06Week/SeSAC_06Week/Extensions/UIFont+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  UIFont+Extensions.swift
+//  SeSAC_06Week
+//
+//  Created by 지영 on 2021/11/01.
+//
+
+import Foundation
