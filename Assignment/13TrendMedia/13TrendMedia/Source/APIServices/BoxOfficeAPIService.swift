765eeae4af1db95e1a7104a490d438570ffedabe
jiyoung <yangg0228@gmail.com>
2021-11-03 20:20:04 +0900

    update Assignment25

---
+++ Assignment/13TrendMedia/13TrendMedia/Source/APIServices/BoxOfficeAPIService.swift
@@ -0,0 +1,8 @@
+//
+//  BoxOfficeAPIService.swift
+//  13TrendMedia
+//
+//  Created by 지영 on 2021/11/03.
+//
+
+import Foundation
---
+++ Assignment/13TrendMedia/13TrendMedia/Source/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  13TrendMedia
+//
+//  Created by 지영 on 2021/11/03.
+//
+
+import Foundation
