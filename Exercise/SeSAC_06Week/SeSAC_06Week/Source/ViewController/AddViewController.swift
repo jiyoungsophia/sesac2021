b67fed0449bcb5d6334f088dc5665bc7dd8ffa40
jiyoung <yangg0228@gmail.com>
2021-11-03 18:27:28 +0900

    update Exercise26

--- Exercise/SeSAC_06Week/SeSAC_06Week/Source/ViewController/AddViewController.swift
+++ Exercise/SeSAC_06Week/SeSAC_06Week/Source/ViewController/AddViewController.swift
@@ -11,6 +11,7 @@ import RealmSwift
 class AddViewController: UIViewController {
     @IBOutlet weak var addTitle: UITextField!
     @IBOutlet weak var addTextView: UITextView!
+    @IBOutlet weak var contentImageView: UIImageView!
     
     let localRealm = try! Realm()
     
@@ -43,7 +44,36 @@ class AddViewController: UIViewController {
         let task = UserDiary(diaryTitle: addTitle.text!, content: addTextView.text!, writeDate: Date(), regDate: Date())
         try! localRealm.write {
             localRealm.add(task)
+            saveImageToDocumentDirectory(imageName: "\(task._id).jpeg", image: contentImageView.image!)
         }
     }
-
+    
+    func saveImageToDocumentDirectory(imageName: String, image: UIImage) {
+        // 1. 이미지 저장할 경로 설정: 도큐먼트 폴더(.documentDirectory), FileManager
+        guard let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return }
+        
+        // 2. 이미지 파일 이름 & 최종 경로 설정
+        let imageURL = documentDirectory.appendingPathComponent(imageName)
+        
+        // 3. 이미지 압축
+        guard let data = image.jpegData(compressionQuality: 0.2) else { return }
+        
+        // 4. 이미지 저장: 동일한 경로에 이미지를 저장하게 될 경우, 덮어쓰기(원래는 자동으로 된다고,,)
+        // 4-1. 이미지 경로 여부 확인
+        if FileManager.default.fileExists(atPath: imageURL.path){
+            //4-2. 기존경로에 있는 이미지 삭제(원래 자동으로 삭제됨)
+            do {
+                try FileManager.default.removeItem(at: imageURL)
+                print("이미지 삭제 완")
+            } catch {
+                print("이미지 삭제하지 못했습니다")
+            }
+        }
+        // 5. 이미지 저장
+        do {
+            try data.write(to: imageURL)
+        } catch {
+            print("이미지를 저장하지 못했습니다")
+        }
+    }
 }
--- Exercise/SeSAC_06Week/SeSAC_06Week/Source/ViewController/SearchViewController.swift
+++ Exercise/SeSAC_06Week/SeSAC_06Week/Source/ViewController/SearchViewController.swift
@@ -32,6 +32,41 @@ class SearchViewController: UIViewController {
         
         searchTableView.reloadData()
     }
+    
+    // 도큐먼트 경로 -> 이미지 찾기 -> UIImage -> UIImageView
+    func loadImageFromDocumentDirectory(imageName: String) -> UIImage? {
+        
+        let documentDirectory = FileManager.SearchPathDirectory.documentDirectory
+        let userDomainMask = FileManager.SearchPathDomainMask.userDomainMask
+        let path = NSSearchPathForDirectoriesInDomains(documentDirectory, userDomainMask, true)
+        
+        if let directoryPath = path.first {
+            let imageURL = URL(fileURLWithPath: directoryPath)
+                .appendingPathComponent(imageName)
+            return UIImage(contentsOfFile: imageURL.path)
+        }
+        return nil
+    }
+    
+    func deleteImageFromDocumentDirectory(imageName: String) {
+        // 1. 이미지 저장할 경로 설정: 도큐먼트 폴더(.documentDirectory), FileManager
+        guard let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return }
+        
+        // 2. 이미지 파일 이름 & 최종 경로 설정
+        let imageURL = documentDirectory.appendingPathComponent(imageName)
+               
+        // 4. 이미지 저장: 동일한 경로에 이미지를 저장하게 될 경우, 덮어쓰기(원래는 자동으로 된다고,,)
+        // 4-1. 이미지 경로 여부 확인
+        if FileManager.default.fileExists(atPath: imageURL.path){
+            //4-2. 기존경로에 있는 이미지 삭제(원래 자동으로 삭제됨)
+            do {
+                try FileManager.default.removeItem(at: imageURL)
+                print("이미지 삭제 완")
+            } catch {
+                print("이미지 삭제하지 못했습니다")
+            }
+        }
+    }
 
 }
 
@@ -50,6 +85,7 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
         cell.searchTitle.text = row.diaryTitle
         cell.searchDate.text = "\(row.writeDate)"
         cell.searchContent.text = row.content
+        cell.searchImageView.image = loadImageFromDocumentDirectory(imageName: "\(row._id).jpeg")
         
         return cell
     }
@@ -58,5 +94,44 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
         return UIScreen.main.bounds.height / 7
     }
     
+    // 본래는 화면 전환 + 값 전달 후 새로운 화면에서 수정이 적합
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let taskToUpdate = tasks[indexPath.row]
+        //1. 수정 - 레코드에 대한 값 수정
+        try! localRealm.write{
+            taskToUpdate.diaryTitle = "새로 수정"
+            taskToUpdate.content = "본래는 화면 전환 + 값 전달 후 새로운 화면에서 수정이 적합"
+            tableView.reloadData()
+        }
+        //2. 일괄 수정
+//        try! localRealm.write{
+//            tasks.setValue("일괄 수정", forKey: "content")
+//            tableView.reloadData()
+//        }
+        //3. 수정: pk 기준으로 수정할 때 사용(권장하지않음) 밸류 넣은값 수정하면서 나머지 초기화
+//        try! localRealm.write {
+//            let update = UserDiary(value: ["_id": taskToUpdate._id, "diaryTitle": "제목만 바꾸기"])
+//            localRealm.add(update, update: .modified)
+//            tableView.reloadData()
+//        }
+        //4.
+//        try! localRealm.write {
+//            localRealm.create(UserDiary.self, value: ["_id": taskToUpdate._id, "diaryTitle": "제목만 바꾸기"], update: .modified)
+//            tableView.reloadData()
+//        }
+        
+    }
     
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        let row = tasks[indexPath.row]
+
+        
+        try! localRealm.write{
+            // 순서 중요
+            deleteImageFromDocumentDirectory(imageName: "\(row._id).jpeg")
+            localRealm.delete(row)
+            tableView.reloadData()
+        }
+    }
 }
