363a3660cfdeadc62ee2cb5398db0111315c6a85
jiyoungsophia <yangg0228@gmail.com>
2021-12-16 17:04:15 +0900

    update Exercise56

--- Exercise/SeSAC_12Week/SeSAC_12Week/AppDelegate.swift
+++ Exercise/SeSAC_12Week/SeSAC_12Week/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseMessaging
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,7 +16,36 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     //1
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         print(#function)
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
+
+        if #available(iOS 10.0, *) {
+          // For iOS 10 display notification (sent via APNS)
+          UNUserNotificationCenter.current().delegate = self
+
+          let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+          UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: { _, _ in }
+          )
+        } else {
+          let settings: UIUserNotificationSettings =
+            UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+          application.registerUserNotificationSettings(settings)
+        }
+
+        application.registerForRemoteNotifications()
+
+        // 메시지 대리자 설정
+        Messaging.messaging().delegate = self
+        
+        Messaging.messaging().token { token, error in
+          if let error = error {
+            print("Error fetching FCM registration token: \(error)")
+          } else if let token = token {
+            print("FCM registration token: \(token)")
+          }
+        }
+        
         return true
     }
 
@@ -38,3 +69,49 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+    func application(application: UIApplication,
+                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+      Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    //포그라운드 수신: willPresent(로컬/푸시 동일)
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.list, .banner, .badge, .sound])
+    }
+    
+    //사용자가 로컬/푸시를 클릭했을 때 Reponse 호출 메서드
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        print("푸시 클릭")
+        //userInfo: key-1(광고), 2(채팅방), 3(사용자 설정)
+        print(response.notification.request.content.userInfo)
+        print(response.notification.request.content.body)
+
+        let userInfo = response.notification.request.content.userInfo
+        if userInfo[AnyHashable("key")] as? Int == 1 {
+            print("광고푸시")
+        } else {
+            print("다른 푸시")
+        }
+        
+        
+        completionHandler()
+    }
+}
+
+extension AppDelegate: MessagingDelegate {
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+      print("Firebase registration token: \(String(describing: fcmToken))")
+
+      let dataDict: [String: String] = ["token": fcmToken ?? ""]
+      NotificationCenter.default.post(
+        name: Notification.Name("FCMToken"),
+        object: nil,
+        userInfo: dataDict
+      )
+    }
+    
+    
+}
